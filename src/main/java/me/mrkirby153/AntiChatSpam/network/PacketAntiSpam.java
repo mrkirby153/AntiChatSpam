83683601125a42747cef90b55afd0dd9d5ce09e9
mrkirby153 <mr.austinwhyte@gmail.com>
2015-09-07 13:06:56 -0700

    Send IChatComponents and display and delete message

--- src/main/java/me/mrkirby153/AntiChatSpam/network/PacketAntiSpam.java
+++ src/main/java/me/mrkirby153/AntiChatSpam/network/PacketAntiSpam.java
@@ -5,7 +5,9 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.IChatComponent;
 
 public class PacketAntiSpam implements IMessage {
 
@@ -13,30 +15,39 @@ public class PacketAntiSpam implements IMessage {
 
     }
 
-    public String message;
+    public IChatComponent message;
 
     public PacketAntiSpam() {
 
     }
 
-    public PacketAntiSpam(String message) {
+    public PacketAntiSpam(IChatComponent message) {
         this.message = message;
     }
 
     @Override
     public void fromBytes(ByteBuf buf) {
-        message = ByteBufUtils.readUTF8String(buf);
+        message = readChatComponent(buf);
+    }
+
+    public IChatComponent readChatComponent(ByteBuf data){
+        return IChatComponent.Serializer.func_150699_a(ByteBufUtils.readUTF8String(data));
+    }
+
+    public void writeChatComponent(ByteBuf data, IChatComponent component){
+        ByteBufUtils.writeUTF8String(data, IChatComponent.Serializer.func_150696_a(component));
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
-        ByteBufUtils.writeUTF8String(buf, this.message);
+        writeChatComponent(buf, this.message);
     }
 
 
     public static class Handler implements IMessageHandler<PacketAntiSpam, IMessage>{
         @Override
         public IMessage onMessage(PacketAntiSpam message, MessageContext ctx) {
+            Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessageWithOptionalDeletion(message.message, 7706071);
             return null;
         }
     }
