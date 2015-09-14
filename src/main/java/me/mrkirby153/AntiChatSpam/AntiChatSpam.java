04689f3b47b3d52f10171b7cbdf731a8650d3bfe
mrkirby153 <mr.austinwhyte@gmail.com>
2015-09-14 12:10:59 -0700

    Added logger and ChatHandler initialization

--- src/main/java/me/mrkirby153/AntiChatSpam/AntiChatSpam.java
+++ src/main/java/me/mrkirby153/AntiChatSpam/AntiChatSpam.java
@@ -8,16 +8,25 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
 import me.mrkirby153.AntiChatSpam.network.PacketAntiSpam;
+import me.mrkirby153.AntiChatSpam.regex.ChatHandler;
 import me.mrkirby153.AntiChatSpam.test.ChatListener;
 import net.minecraftforge.common.MinecraftForge;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "AntiChatSpam", name = "Anti Chat Spam", version = "@VERSION@")
 public class AntiChatSpam {
 
     public static SimpleNetworkWrapper network;
 
+    @Mod.Instance("AntiChatSpam")
+    public static AntiChatSpam instance;
+
+    public Logger logger;
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        logger = LogManager.getLogger("AntiChatSpam");
         network = NetworkRegistry.INSTANCE.newSimpleChannel("AntiChatSpam");
         network.registerMessage(PacketAntiSpam.Handler.class, PacketAntiSpam.class, 0, Side.CLIENT);
     }
@@ -27,6 +36,6 @@ public class AntiChatSpam {
         ChatListener cl = new ChatListener();
         FMLCommonHandler.instance().bus().register(cl);
         MinecraftForge.EVENT_BUS.register(cl);
+        ChatHandler.load();
     }
-
 }
