76e25985cc334cf57d767ee5a8246aaef3811bae
mrkirby153 <mr.austinwhyte@gmail.com>
2015-09-14 13:59:20 -0700

    Moved package to a more logincal one

--- src/main/java/me/mrkirby153/AntiChatSpam/listener/ChatListener.java
+++ src/main/java/me/mrkirby153/AntiChatSpam/listener/ChatListener.java
@@ -1,4 +1,4 @@
-package me.mrkirby153.AntiChatSpam.test;
+package me.mrkirby153.AntiChatSpam.listener;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
@@ -6,16 +6,17 @@ import cpw.mods.fml.relauncher.Side;
 import me.mrkirby153.AntiChatSpam.regex.ChatHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.IChatComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+
+import java.util.Random;
 
 public class ChatListener {
 
-    private Minecraft mc = null;
+    private static final Random random = new Random();
 
     @SubscribeEvent
     public void handleChat(ClientChatReceivedEvent event) {
-
+        event.setCanceled(ChatHandler.hasHandledChat(event.message));
     }
 
     @SubscribeEvent
@@ -23,15 +24,8 @@ public class ChatListener {
         if (event.side == Side.CLIENT) {
             if (event.phase == TickEvent.Phase.START) {
                 if (Minecraft.getMinecraft() != null && Minecraft.getMinecraft().thePlayer != null)
-                    Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText("THIS IS A TEST!"));
+                    Minecraft.getMinecraft().thePlayer.addChatComponentMessage(new ChatComponentText("Test "+random.nextInt(Integer.MAX_VALUE)));
             }
-        }
-    }
-
-    public void addChatComponentMessage(IChatComponent p_146105_1_) {
-        if (ChatHandler.hasHandledChat(p_146105_1_)) {
-            System.out.println("THIS IS A TEST OF THINGS!");
-            return;
         }
     }
 }
