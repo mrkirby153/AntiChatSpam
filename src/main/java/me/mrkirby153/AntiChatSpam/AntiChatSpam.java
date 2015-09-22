c26437b2ef2d70ba557656b1c17f539c6d4959f7
mrkirby153 <mr.austinwhyte@gmail.com>
2015-09-21 20:12:22 -0700

    Don't load mod if not on client

--- src/main/java/me/mrkirby153/AntiChatSpam/AntiChatSpam.java
+++ src/main/java/me/mrkirby153/AntiChatSpam/AntiChatSpam.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.relauncher.Side;
 import me.mrkirby153.AntiChatSpam.listener.ChatListener;
 import me.mrkirby153.AntiChatSpam.reference.Strings;
 import me.mrkirby153.AntiChatSpam.regex.ChatHandler;
@@ -24,9 +25,14 @@ public class AntiChatSpam {
 
     public static Logger logger;
     private VersionChecker checker;
+    public static boolean isClient = true;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
+        if(event.getSide() == Side.SERVER) {
+            logger.info("Wrong side SERVER, AntiChatSpam not loading");
+            isClient = false;
+        }
         logger = LogManager.getLogger("AntiChatSpam");
         network = NetworkRegistry.INSTANCE.newSimpleChannel("AntiChatSpam");
         checker = new VersionChecker();
@@ -36,6 +42,8 @@ public class AntiChatSpam {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event){
+        if(!isClient)
+            return;
         ChatListener cl = new ChatListener();
         FMLCommonHandler.instance().bus().register(cl);
         MinecraftForge.EVENT_BUS.register(cl);
